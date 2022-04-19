ec35b05b41c8b65e98289a7ef06c2e371b979e93
ipstepaniuk@gmail.com <ipstepaniuk@gmail.com>
2022-04-19 21:45:18 +0300

    Comit3

---
+++ Assets/Scripts/Calculator.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Calculator : MonoBehaviour
+{
+    public Text Text1;
+    public string calc1;
+    public string x1;
+}
